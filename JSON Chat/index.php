54b7d9fcd298f32fae3385a357f1881bd289d17a
Michael Snow Carini <65109289+NorthChild@users.noreply.github.com>
2021-05-09 11:27:34 +0200

    Update index.php

--- JSON Chat/index.php
+++ JSON Chat/index.php
@@ -24,10 +24,13 @@
 
 <html>
 <head>
+  <title>Week 4 - JSON Chat</title>
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 </head>
 <body>
+
       <h1>Chat</h1>
+
       <form method="post" action="index.php">
       <p>
       <input type="text" name="message" size="60"/>
@@ -36,30 +39,42 @@
       <a href="chatlist.php" target="_blank">chatlist.php</a>
       </p>
       </form>
+
       <div id="chatcontent">
           <img src="img/spinner.gif" alt="Loading..."/>
       </div>
 
 <script type="text/javascript">
+
 function updateMsg() {
+
   window.console && console.log('Requesting JSON');
-  $.getJSON('chatlist.php', function(rowz){
+
+  $.getJSON('chatlist.php', function(data){
+
       window.console && console.log('JSON Received');
-      window.console && console.log(rowz);
+      window.console && console.log(data);
+
       $('#chatcontent').empty();
-      for (var i = 0; i < rowz.length; i++) {
-        arow = rowz[i];
-        $('#chatcontent').append('<p>'+arow[0] +
-            '<br/>&nbsp;&nbsp;'+arow[1]+"</p>\n");
+
+      for (var i = 0; i < data.length; i++) {
+
+        row = data[i];
+        $('#chatcontent').append('<p>'+row[0] +
+            '<br/>&nbsp;&nbsp;'+row[1]+"</p>\n");
       }
+
       setTimeout('updateMsg()', 1000);
   });
 }
 
 // Make sure JSON requests are not cached
 $(document).ready(function() {
+
   $.ajaxSetup({ cache: false });
   updateMsg();
+
 });
+
 </script>
 </body>
